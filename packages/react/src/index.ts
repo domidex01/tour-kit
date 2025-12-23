4c7278eb69a20c3afd19db9bd27ada9803e0056e
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2025-12-23 11:02:32 +0700

    chore: re-export types from @tour-kit/core in hints and react packages

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,2 @@
-export const VERSION = '0.0.1'
-export const hello = () => 'Hello from @tour-kit/react!'
-export { hello as helloCore } from '@tour-kit/core'
+// Re-export types from core
+export * from '@tour-kit/core'
