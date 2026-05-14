45de69a457e68ef2921f44fc19b6ce3627107c27
domidex01 <usertourkit@gmail.com>
2026-05-14 12:09:18 +0700

    Yandex metrika set up

---
+++ apps/docs/components/analytics/yandex-metrika.tsx
@@ -0,0 +1,21 @@
+import Script from 'next/script'
+
+export function YandexMetrika({ id }: { id: string }) {
+  const numericId = Number(id)
+  return (
+    <>
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${numericId}', 'ym');ym(${numericId}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+      </Script>
+      <noscript>
+        <div>
+          <img
+            src={`https://mc.yandex.ru/watch/${numericId}`}
+            style={{ position: 'absolute', left: '-9999px' }}
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
+  )
+}
